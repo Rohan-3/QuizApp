77f61a373b852c79fb1f566cffe679144a1b28c5
vaishnavi04015 <153651617+vaishnavi04015@users.noreply.github.com>
2023-12-15 07:08:30 +0530

    admin page

---
+++ admin.js
@@ -0,0 +1,10 @@
+const update=()=>
+{
+    document.getElementById("update").style="display:flex; column-gap:20px";
+    document.getElementById("result").style.display="none";
+}
+const result=()=>
+{
+    document.getElementById("result").style="display:flex; column-gap:20px";
+    document.getElementById("update").style.display="none";
+}
