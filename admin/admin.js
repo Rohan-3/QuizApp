84fa6177578b06f259abe83154e169954db50c05
vaishnavi04015 <153651617+vaishnavi04015@users.noreply.github.com>
2023-12-16 14:22:22 +0530

    css update for admin page

--- admin/admin.js
+++ admin/admin.js
@@ -2,13 +2,11 @@ const updateExam=()=>
 {
     document.getElementById("update").style="display:flex; column-gap:20px";
     document.getElementById("result").style.display="none";
-    document.getElementById("logo").style.display="none";
 }
 const result=()=>
 {
     document.getElementById("result").style="display:flex; column-gap:20px";
     document.getElementById("update").style.display="none";
-    document.getElementById("logo").style.display="none";
 }
 const adddiv=()=>
 {
