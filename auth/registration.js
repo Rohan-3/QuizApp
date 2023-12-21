2400260b02f4e03a11cf01bbdf9f4a0c3e7ed04c
vaishnavi04015 <153651617+vaishnavi04015@users.noreply.github.com>
2023-12-21 15:22:25 +0530

    solved registration issues

--- auth/registration.js
+++ auth/registration.js
@@ -1,5 +1,5 @@
-//let students=[];
-//students=JSON.parse(localStorage.getItem("s"))||[];
+let students =[];
+students=JSON.parse(localStorage.getItem("s"))||[];
 let flag=true;
 let pic;
 const show_Img=()=>
@@ -28,35 +28,48 @@ const pssd=()=>
 
 }
 
+const filled=()=>
+{
+    let f = ["name","password","re_password"]
+    for(let i of f)
+    {
+        let value=document.getElementById(i).value.trim();
+        if(value==="")
+        {
+            alert(`please fill in ${i}`);
+            return false;
+        }
+    }
+    let pic = document.getElementById("myfile");
+    if(!pic.files.length)
+    {
+        alert("please upload your photo");
+        return false;
+    }
+    return true;
+}
+
 const signup=()=>
 {
-    if(flag==true)
+    if(!filled())
     {
-        let k = JSON.parse(localStorage.getItem("s"));
-        let students =[];
-        let name = document.getElementById("t1").value;
-        let roll = students.length > 0 ? students[students.length - 1].roll + 1 : 1;
-        let pssd = document.getElementById("t3").value;
-        let image= document.getElementById("pic").src;
-        let user= {name,roll,pssd,image};
-        if(k==null)
-        {
-            students=[...students,user];
-        }
-        else
-        {
-            students=k;
-            students=[...students,user];
-        }
-        localStorage.setItem("s",JSON.stringify(students));
-        alert(`Registered successfully and your roll no is ${roll}`);
-        
-       }
-    else
-    {
-        let pssd1 = document.getElementById("t3").value ;
-        let pssd2 = document.getElementById("t4").value;
-        if(pssd1.length>0 && pssd2.length>0)
-        alert("Password did not match, please re-enter password");
+        return;
     }
+    let pssd1 = document.getElementById("password").value;
+    let pssd2 = document.getElementById("re_password").value;
+    if(pssd1!=pssd2)
+    {
+        alert("Password did not match, please re-enter password");
+        return;
+    }
+    let name = document.getElementById("name").value;
+    let roll = students.length > 0 ? students[students.length - 1].roll + 1 : 1;
+    let pssd = document.getElementById("password").value;
+    let image = document.getElementById("pic").src;
+    let user = {name,roll,pssd,image};
+    students=[...students,user];
+    localStorage.setItem("s",JSON.stringify(students));
+    alert(`Registered successfully and your roll no is ${roll}`);
+    document.getElementById("reg").action="index.html";
+    
 }
