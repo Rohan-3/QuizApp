77dd68f8a518ac6d9f6e4585d6f245c47b0ad494
K-Sruti <ksruti332@gmail.com>
2023-12-26 17:15:21 +0530

    Certification functional done

--- exam/certificate.js
+++ exam/certificate.js
@@ -7,26 +7,12 @@ const download = () =>{
     html2pdf().from(certificate).save();
 }
 
-let marks  = JSON.parse(localStorage.getItem("marks"))
-let data  = JSON.parse(localStorage.getItem("question"))
 const certificateData =() =>{
-    let result=JSON.parse(localStorage.getItem("result"))||[];
-    
-    let name = result.name
-    console.log(marks);
-    document.getElementById("marks").innerHTML = marks;
+    let data = JSON.parse(localStorage.getItem("question"));
+    let result = JSON.parse(localStorage.getItem("allresult"));
+    let display = result[result.length-1];
+    document.getElementById("marks").innerHTML = display.marks;
     document.getElementById("qnos").innerHTML = data.length;
-    document.getElementById("name").innerHTML = name;
-
-    if (markGot === queLength) {
-        grade = "Grade: A+"
-      }else if (markGot <= queLength &&  markGot>= queLength/2) {
-        grade = "Grade: B+"
-      }else if (markGot > 0 && markGot< queLength/2) {
-        grade = "Grade: C+"
-      }else{
-        grade = "Grade: D FAIL"
-      }
-    document.getElementById("grade").innerHTML = grade;
-    
-}
+    document.getElementById("name").innerHTML = display.name;
+    document.getElementById("grade").innerHTML = display.grade;
+    }
